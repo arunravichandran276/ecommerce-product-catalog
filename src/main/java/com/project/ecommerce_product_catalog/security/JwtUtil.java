b49a008a4e413861d79e92223b738ac90eb9918c
arunravichandran276 <arunravichandran2k3@gmail.com>
2025-08-11 12:02:56 +0530

    updated the utility packages

--- src/main/java/com/project/ecommerce_product_catalog/security/JwtUtil.java
+++ src/main/java/com/project/ecommerce_product_catalog/security/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.project.ecommerce_product_catalog.security;
 
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -22,5 +24,26 @@ public class JwtUtil {
                 .setSubject(username)
                 .compact();
     }
+    public String extractUsername(String token){
+        return extractClaim(token,claim->claim.getSubject());
+    }
+    public <T> T extractClaim(String token,Function<Claims,T> claimResolver){
+        Claims claims=extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
 
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    public boolean validJwtToken(String token,String username){
+        return username.equals(extractUsername(token)) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaim(token,claims -> claims.getExpiration()).before(new Date());
+    }
 }
